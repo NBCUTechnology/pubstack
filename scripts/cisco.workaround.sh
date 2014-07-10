e412c1dcf640c65346b13bf8dad79d82cb78a542
Eric J. Duran <eric.duran7@gmail.com>
2014-07-10 18:10:30 -0400

    Simplify command Thanks @conortm

--- scripts/cisco.workaround.sh
+++ scripts/cisco.workaround.sh
@@ -2,4 +2,4 @@
 # Alters mac Firewall to remove the cisco "deny ip from any ip rule"
 # ./cisco.workaround.sh
 printf "Asking for root access to remove Cisco IP Blocking rule\n"
-sudo ipfw -a list | grep -n "deny ip from any to any" | awk '{ print $1}' | awk 'BEGIN {FS=":"} ; {print $2}' | xargs -0 sudo ipfw delete
+sudo ipfw -a list | grep "deny ip from any to any" | awk '{ print $1}' | xargs -0 sudo ipfw delete
